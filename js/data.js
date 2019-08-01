719d7b89c8021eb031d73883c1e2e2111d1066f9
Jan Hořava <czechinthemail@gmail.com>
2019-07-31 21:40:12 -0700

    added audio play button and duration setting

--- js/data.js
+++ js/data.js
@@ -1,1 +1,14 @@
 'use strict';
+
+function play_audio(){
+    audio_create({
+      'audios': {
+        'boop': {
+          'duration': core_storage_data['duration'],
+        },
+      },
+    });
+    audio_start({
+      'id': 'boop',
+    });
+}
--- js/main.js
+++ js/main.js
@@ -2,8 +2,18 @@
 
 function repo_init(){
     core_repo_init({
+      'events': {
+        'play-audio': {
+          'onclick': play_audio,
+        },
+      },
+      'info': '<input id=play-audio type=button value="Play Audio">',
       'menu': true,
       'menu-block-events': false,
+      'storage': {
+        'duration': .1,
+      },
+      'storage-menu': '<table><tr><td><input id=duration><td>Duration</table>',
       'title': 'AudioEditor.htm',
     });
 }
