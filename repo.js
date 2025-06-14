b98e056b79b6237a1c7b85319f6965ae26dd793f
Jan Hořava <czechinthemail@gmail.com>
2025-06-14 01:25:06 -0700

    simplified property usage

--- repo.js
+++ repo.js
@@ -10,10 +10,10 @@ function play_audio(){
 
     audio_create({
       'test': {
-        'duration': core_storage_data['duration'],
-        'frequency': core_storage_data['frequency'],
-        'type': core_storage_data['type'],
-        'volume': core_storage_data['volume'],
+        'duration': core_storage_data.duration,
+        'frequency': core_storage_data.frequency,
+        'type': core_storage_data.type,
+        'volume': core_storage_data.volume,
       },
     });
     audio_start('test');
